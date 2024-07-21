180349ee8b9403844a8e747b201ef69954905fe4
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-07-22 01:14:15 +0300

    update

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-# import torch
-
-# if torch.backends.mps.is_available():
-#     device = torch.device("mps")
-#     x = torch.ones(1, device=device)
-#     print(x)
-# else:
-#     print("MPS device not found.")
-
-
-# import torch
-# from diffusers import StableDiffusionPipeline
-
-# # Check if MPS is available
-# mps_available = hasattr(torch.backends, "mps") and torch.backends.mps.is_available()
-
-# # Set the device
-# device = torch.device("mps" if mps_available else "cpu")
-# print(f"Using device: {device}")
-
-# # Load the pipeline
-# pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float32)
-# pipe = pipe.to(device)
-
-# # Generate an image
-# prompt = "a photo of an astronaut riding a horse on mars"
-# image = pipe(prompt, num_inference_steps=20).images[0]  
-
-# # Save the image
-# image.save("astronaut_on_mars.png")
-
 from diffusers import StableDiffusionPipeline
 import torch
 
